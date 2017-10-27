299aab8387538ebe85498e84fcc18897ef866471
botelho-rj <jpaulo_jpcb@hotmail.com>
2017-10-27 17:44:35 -0200

    Resolvendo problemas

--- SportsGuyNet/IdentityConfig.cs
+++ SportsGuyNet/IdentityConfig.cs
@@ -4,18 +4,14 @@ using Microsoft.Owin.Security.Cookies;
 using Owin;
 using SportsGuyNet.DAL;
 using SportsGuyNet.Infraestrutura;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace SportsGuyNet
 {
-  public class IdentityConfig
+    public class IdentityConfig
   {
     public void Configuration(IAppBuilder app)
     {
-      app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+      app.CreatePerOwinContext(IdentityDbContextAplicacao.Create);
       app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
       app.CreatePerOwinContext<GerenciadorPapel>(GerenciadorPapel.Create);
       app.UseCookieAuthentication(new CookieAuthenticationOptions {
